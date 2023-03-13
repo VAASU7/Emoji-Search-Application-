d1f7680995cc4448f00b97e4d04448d78d59951b
VAASU7 <112189905+VAASU7@users.noreply.github.com>
2023-03-13 21:38:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+import './App.css'
 
-function App() {
+
+function App  () {
+  let[data,setData] = useState([])
+  let[search ,setSearch] = useState('')
+
+  useEffect(() =>{
+    fetch('https://emoji-api.com/emojis?access_key=80f69dabb82f0487cef541e88c0ec7eb3c1bdf3f')
+    .then(res => res.json())
+    .then(res => setData(res))
+
+  },[])
+  let handleSearch = (e) =>{
+    setSearch(e.target.value)
+  }
+
+  let handlSubmit = () =>{
+    if (search !== ''){
+      fetch(`https://emoji-api.com/emojis?search=${search}&access_key=80f69dabb82f0487cef541e88c0ec7eb3c1bdf3f`)
+    .then(res => res.json())
+    .then(res => setData(res))
+
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='menu'>
+      <div className='menu-items'>
+      <h1>Emoji Search</h1>
+      <p> A Sipmle Emoji Search App</p>
+      <input type='text' placeholder='Emoji Name' value={search} onChange={(e) => handleSearch(e)}></input>
+      <button onClick={() => handlSubmit()}>Search</button>
+      </div>
+      </div>
+     
+      <div className='container'>
+        {
+          data.map((e,i) => 
+          <div className='card' key={e.slug}>
+          <p className='emoji'>{e.character}</p>
+          <p className='name'>{e.unicodeName}</p>
+        </div>
+          )
+        }
+      </div>
+      
     </div>
   );
 }
 
-export default App;
+export default App
